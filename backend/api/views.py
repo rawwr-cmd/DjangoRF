fd4ad5a371982352a478db10d830b0892b93e19f
rawwr <akshayace0302@gmail.com>
2022-05-15 20:16:13 +0530

    Rest Framework View and Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,21 +1,17 @@
 # converting string into actual pyhon dictionary
-import json
 from django.forms.models import model_to_dict
-from django.http import  JsonResponse, HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
 
-
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
     # getting a random product from products.models
+    ''' DRF API VIEW '''
     model_data = Product.objects.all().order_by('?').first()
     data={}
     if model_data:
         data = model_to_dict(model_data, fields = ['id', 'title', 'price'])
-        
-    return JsonResponse(data)
 
-    #     print(data)
-    #     data = dict(data)
-    #     json_data_str = json.dumps(data)
-    # return HttpResponse(json_data_str, headers={'Content-Type': 'application/json'})
+    return Response(data)
