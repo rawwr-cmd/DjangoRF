0aec234c902537ad3553d149dca6ff4fe1c82aa0
rawwr <akshayace0302@gmail.com>
2022-05-15 19:03:55 +0530

    Django model instance to dict

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,7 @@
 # converting string into actual pyhon dictionary
 import json
-from django.http import  JsonResponse
+from django.forms.models import model_to_dict
+from django.http import  JsonResponse, HttpResponse
 
 from products.models import Product
 
@@ -10,8 +11,11 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by('?').first()
     data={}
     if model_data:
-        data['id'] = model_data.id                         # id-by_default
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        data = model_to_dict(model_data, fields = ['id', 'title', 'price'])
+        
     return JsonResponse(data)
+
+    #     print(data)
+    #     data = dict(data)
+    #     json_data_str = json.dumps(data)
+    # return HttpResponse(json_data_str, headers={'Content-Type': 'application/json'})
--- py_client/basic.py
+++ py_client/basic.py
@@ -6,7 +6,11 @@ endpoint = 'http://localhost:8000/api/'
 
 # HTTP response
 get_response = requests.get(endpoint,  json={'product_id': 123})
+# print(get_response.headers)
+# print('...')
+# print(get_response.text)
 #  print raw text response
 # print(get_response.text)
+
 print(get_response.json())          # python dict
 # print(get_response.status_code)
