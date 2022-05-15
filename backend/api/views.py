085d2dbbff7d16584f5fb41eee9f4dd3adb2c4e2
rawwr <akshayace0302@gmail.com>
2022-05-15 16:23:39 +0530

    Django model instance as API response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,24 @@
+# converting string into actual pyhon dictionary
+import json
+
 from django.http import  JsonResponse
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({'message' : 'Hi there, this is Django api response'})
+    # print(dir(request))
+    print(request.GET)
+    print(request.POST)
+    body = request.body #byte string of JSON data
+    data = {}
+    try: 
+        data = json.loads(body)  #String of JSON data -> Python Dict
+    except:
+        pass
+    print(data)    
+    # data['headers'] = request.headers # request.META
+
+    data['params'] = dict(request.GET) 
+    data['header'] = dict(request.headers)
+    data['content-type'] = request.content_type
+    # print(body)         #output b'{"query": "hello world"}'
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,8 +5,8 @@ import requests
 endpoint = 'http://localhost:8000/api/'
 
 # HTTP response
-get_response = requests.get(endpoint, json={'query':'hello world'})
+get_response = requests.get(endpoint, params={'hi': 'bella'}, json={'query':'hello world'})
 #  print raw text response
 # print(get_response.text)
-print(get_response.json()['message'])          # python dict
+print(get_response.json())          # python dict
 print(get_response.status_code)
