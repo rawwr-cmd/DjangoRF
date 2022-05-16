67bdbfd7b974fde9a43ac823078f47c57562a713
rawwr <akshayace0302@gmail.com>
2022-05-16 10:18:00 +0530

    django rest framework generics RetrieveAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Product 
+from .serializers import ProductSerializer
+
+class ProductDetailsAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    #lookup_field = 'pk'
---
+++ py_client/details.py
@@ -0,0 +1,10 @@
+import requests 
+
+endpoint = 'http://localhost:8000/api/products/1'
+
+# HTTP response
+get_response = requests.get(endpoint)
+
+
+print(get_response.json())          # python dict
+# print(get_response.status_code)
