bd3bd0d1a3f56d9030b9a2be6998c2907c37cb00
rawwr <akshayace0302@gmail.com>
2022-05-16 23:50:35 +0530

    mixins and a generic API view (class-based-view)

--- py_client/details.py
+++ py_client/details.py
@@ -1,10 +1,9 @@
 import requests
 
-endpoint = 'http://localhost:8000/api/products/2/'
+endpoint = 'http://localhost:8000/api/products/10/'
 
 # HTTP response
 get_response = requests.get(endpoint)
-
 
 print(get_response.json())          # python dict
 # print(get_response.status_code)
